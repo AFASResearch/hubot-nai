8044af5d06754e653789e807605c8a7607d5dd0f
Johan Gorter <jgo@AFASs-MacBook-Pro.local>
2016-12-05 22:21:07 +0100

    stubbing auth

--- src/main.js
+++ src/main.js
@@ -1,8 +1,16 @@
-let temperature = require('./temperature');
+var temperature = require('./temperature');
 
 module.exports = function(robot) {
 
-  robot.hear(/(temperature)|(temperatuur)|(warm)|(koud)|(heet)/i, (res) => {
+  // hubot-auth is a mess in combination with hubot-slack, stubbing all authorization right now
+  // see https://github.com/slackapi/hubot-slack/issues/352
+  robot.auth = {
+    hasRole: function() {
+      return true;
+    }
+  };
+
+  robot.hear(/(temperature)|(temperatuur)|(warm)|(koud)|(heet)/i, function(res) {
     res.send('Het is nu ' + (Math.round(temperature.getTemperature() * 100) / 100) + '°C');
-  })
+  });
 }
