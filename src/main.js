bbf0ace258741a6dab52be330b7496a7dc8e808c
Johan Gorter <johan-gorter@users.noreply.github.com>
2016-12-12 17:03:01 +0100

    hear -> respond

--- src/main.js
+++ src/main.js
@@ -10,7 +10,7 @@ module.exports = function(robot) {
     }
   };
 
-  robot.hear(/(temperature)|(temperatuur)|(warm)|(koud)|(heet)/i, function(res) {
+  robot.respond(/(temperature)|(temperatuur)|(warm)|(koud)|(heet)/i, (res) => {
     res.send('Het is nu ' + (Math.round(temperature.getTemperature() * 100) / 100) + '°C');
   });
 }
